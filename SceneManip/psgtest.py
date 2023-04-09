f7888d5760f630ba0c99a38eeab87a5e247e1c53
Ashroom66 <ashroom09@gmail.com>
2023-04-09 19:03:31 +0900

    feat: simpleGUI 入力処理

--- SceneManip/psgtest.py
+++ SceneManip/psgtest.py
@@ -2,9 +2,9 @@
 # 参考：https://fuji-pocketbook.net/pysimplegui/
 import PySimpleGUI as sg
 
-layout = [  [sg.Text("1行目")],
-            [sg.Text("2行目")],
-            [sg.Text("3行目"), sg.Text("これも3行目")]
+layout = [  [sg.Text("name?"), sg.Input()],
+            [sg.Text("sex?"), sg.Input("mare")],
+            [sg.Button("Confirm"), sg.Button("Exit"), sg.Button(), sg.Button('はい', key='-YES-')]
         ]
 
 window = sg.Window("title", layout)
@@ -12,3 +12,16 @@ window = sg.Window("title", layout)
 event, values = window.read()
 window.close()
 
+print(f"event:\t{event}")
+print(f"values:\t{values}")
+# event:  Confirm
+# values: {0: 'aaaaa', 1: 'mare'}
+
+# event:  Exit
+# values: {0: 'asdfas', 1: 'mare'}
+
+# event:
+# values: {0: '', 1: 'mare'}
+
+# event:  -YES-
+# values: {0: 'aaaaa', 1: 'mare'}
