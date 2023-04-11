dc2e6cf949486db271bacc49ac1ca388f5038dbd
Ashroom66 <ashroom09@gmail.com>
2023-04-11 13:03:22 +0900

    feat: xmlView, XMLファイルを読み込み、レンダーボタンを押すとレンダリングして結果を表示する

--- SceneManip/xmlView.py
+++ SceneManip/xmlView.py
@@ -1,8 +1,9 @@
 import PySimpleGUI as sg
 import mitsuba as mi
 import numpy as np
+import io
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 # variant
 mi.set_variant('llvm_ad_rgb')
 
@@ -10,18 +11,25 @@ mi.set_variant('llvm_ad_rgb')
 plt.axis('off')
 sg.theme('Green')
 
-def mk_render():
+def mk_renderFig():
     fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
     # ここにMitsubaのレンダリング
     original_image = mi.render(mi.load_file(values["-RenderPath-"]), spp=128)
-    return plt.imshow(original_image ** (1.0/2.2))
+    image = original_image ** (1.0/2.2)
+    plt.imshow(image)
+    return fig
     
 
 
 def update_renderWindow():
     # update sg window
+    fig = mk_renderFig()
+    item = io.BytesIO()
+    plt.savefig(item, format="png")
+    plt.clf()
     imgPanel = window["-Image-"]
-    imgPanel.update(data=mk_render().tobytes())
+    imgPanel.update(data=item.getvalue())
     
 
 render_canvas = [sg.Image(key="-Image-")]
