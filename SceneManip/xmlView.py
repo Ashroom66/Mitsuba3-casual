5e36fe074a419e5d125946c823fc4f578613b6ab
Ashroom66 <ashroom09@gmail.com>
2023-04-12 20:08:40 +0900

    feat: xmlView, 読み込んだXMLファイルからパラメータを抽出(これから表示周りを整備)

--- SceneManip/xmlView.py
+++ SceneManip/xmlView.py
@@ -10,9 +10,14 @@ mi.set_variant('llvm_ad_rgb')
 # Styles
 sg.theme('Green')
 
+# mitsuba data
+scene = ""
+params = ""
+
 def mk_renderFig():
+    global scene, params
     # ここにMitsubaのレンダリング
-    original_image = mi.render(mi.load_file(values["-RenderPath-"]), spp=128)
+    original_image = mi.render(scene, spp=128)
     image = original_image ** (1.0/2.2)
 
     dpi =72
@@ -31,6 +36,8 @@ def update_renderWindow():
     plt.clf()
     imgPanel = window["-Image-"]
     imgPanel.update(data=item.getvalue(),size=(fig.get_size_inches()*fig.dpi))
+    print(fig.get_size_inches())
+    window["-Params-"].set_size=(300, fig.get_size_inches()[1]*fig.dpi)
 
 def update_contentsPanel():
     # パラメータが変わったときに呼び出す
@@ -40,18 +47,24 @@ def update_contentsPanel():
 def read_XML():
     # XMLファイルからパラメータ一覧を取り出す
     # menu_contentsにパラメータ一覧を登録する
-    pass
+    global scene, params, menu_contents, window
+    scene = mi.load_file(values["-RenderPath-"])
+    params = mi.traverse(scene)
+    menu_contents = []
+    for i in params.keys():
+        menu_contents.append([sg.Text(i), sg.InputText(default_text=params[i], key=i)])
+    window["-Params-"].update(menu_contents)
 
 menu_contents = []
 render_canvas = sg.Image(key="-Image-",size=(640,480))
-params_canvas = sg.Column([menu_contents], key="-Params-", size=(300, 480), scrollable=True, background_color="dark green")
+params_canvas = sg.Column(menu_contents, key="-Params-", size=(300, 480), scrollable=True, background_color="dark green", vertical_alignment='top')
 
 layout = [
     [sg.Text("シーンファイル"), sg.InputText(key="-RenderPath-", enable_events=True), sg.FileBrowse(key="-Browse-")],
     [sg.Button("保存(WIP)"), sg.Button("レンダー", key="-Render-")],
-    [render_canvas, params_canvas]
+    [render_canvas, sg.VerticalSeparator(), params_canvas]
 ]
-window = sg.Window("MitsubaXMLviewer(ver.0.01)", layout)
+window = sg.Window("MitsubaXMLviewer(ver.0.2)", layout)
 
 while True:
     event, values = window.read()
